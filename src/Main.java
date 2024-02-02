94a2dc690f34ceadf062873734e5d7f7298a25d0
Mohamed Magan <momagan@gmail.com>
2024-02-02 08:33:07 -0700

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int x = 223;
+        long a = 234578096L;
+        double y = 13.5;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Mo";
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("My number is :" + x);
+        System.out.println("My number is :" + a);
+        System.out.println("My number is :" + y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println(name);
+
     }
-}
+    }
