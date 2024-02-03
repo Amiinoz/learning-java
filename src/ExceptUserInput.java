6050415d1cf3ca686427592c07050ca25b97ed83
Mohamed Magan <momagan@gmail.com>
2024-02-03 10:10:50 -0700

    expressions

--- src/ExceptUserInput.java
+++ src/ExceptUserInput.java
@@ -7,6 +7,19 @@ public class ExceptUserInput {
         System.out.println("What is your name? ");
         String name = scanner.nextLine();
 
+        System.out.println("Waht is your age? ");
+        int age = scanner.nextInt();
+
+        // To prevent the scanner skipping next line we call
+        scanner.nextLine();
+
+        System.out.println("Who is your favorite team?");
+        String footballTeam = scanner.nextLine();
+
+
+
         System.out.println("Hello, " + name);
+        System.out.println("Your are? " + age );
+        System.out.println("Your favorite team is " + footballTeam);
     }
 }
---
+++ src/Experessions.java
@@ -0,0 +1,28 @@
+public class Experessions {
+
+    public static void main(String[] rgs) {
+
+        // Expression = Operands & Operators
+        // Operands = Values, Variables, Numbers, Quantity
+        // Operators = + - * / %
+
+        int friends = 10;
+
+        friends = friends + 1;
+//        short form =>
+        friends++;
+        friends = friends * 2;
+        friends = friends / 2;
+//         gives you the remainder of /
+        friends = friends % 3;
+
+        double friends = 10;
+        friends = (double) friends/3;
+
+
+
+
+        System.out.println(friends);
+    }
+
+}
