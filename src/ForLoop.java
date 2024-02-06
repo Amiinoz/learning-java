cf1b63063cf773c4adda0f1c5d49991de75ef448
Mohamed Magan <momagan@gmail.com>
2024-02-06 10:57:08 -0700

    for loops

---
+++ src/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        // For loop = executes a block of code a limited amount of times
+        // starts with 3 statement
+        // 1. declare statement - int index = 0;
+        // 2. test for condition - index <= 10;
+        // 3. add of subtract - index++
+
+        for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+
+    }
+}
