65143c3675f416e1e0c98a275cd8e03a95ede19d
Mohamed Magan <momagan@gmail.com>
2024-02-06 09:46:57 -0700

    if statements

---
+++ src/IfStatements.java
@@ -0,0 +1,18 @@
+public class IfStatements {
+
+    public static void main(String[] args) {
+
+        // If statements = Executes a block code if conditions are met
+        int age = 18;
+
+        if(age >= 75) {
+            System.out.println("You are elder state man");
+        }
+        else if(age >= 18) {
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not Adult yet!");
+        }
+    }
+}
