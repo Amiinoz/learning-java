07437af800347290d40c044d7f5c9e5bbb5982fb
Mohamed Magan <momagan@gmail.com>
2024-02-07 10:51:00 -0700

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,35 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        // Arrays are used to store multiple values in a single variable
+//
+        String[] cars = {
+                "Tesla",
+                "Camero",
+                "Mustang"
+        };
+
+        cars[0] = "Carasida";
+
+        System.out.println(cars[1]);
+
+        // Another way to declare arrays is
+//
+        String[] cars = new String[3];
+
+        cars[0] = "Camero";
+        cars[1] = "Tesla";
+        cars[2] = "Carasida";
+//
+        System.out.println(cars[2]);
+
+
+        // This is how to itterate trough a arrray using if statement
+
+        for(int i = 0; i<cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+    }
+}
