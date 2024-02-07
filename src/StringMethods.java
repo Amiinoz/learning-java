e710bc2c2e6c83913bf0cf71d892d1f301a47b5f
Mohamed Magan <momagan@gmail.com>
2024-02-07 11:51:52 -0700

    string methods

---
+++ src/StringMethods.java
@@ -0,0 +1,16 @@
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+        // String = a reference data type that can store one or more characters
+        // reference data types have access to useful methods
+
+        String name = "MO";
+
+//        boolean results = name.equals("MO");
+        // or
+        boolean results = name.equalsIgnoreCase("mo");
+
+        System.out.println(results);
+    }
+}
