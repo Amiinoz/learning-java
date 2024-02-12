8f3e883db170edf2227b153df554151663e5aa3d
Mohamed Magan <momagan@gmail.com>
2024-02-12 08:15:01 -0700

    final word in java

---
+++ src/Final.java
@@ -0,0 +1,15 @@
+public class Final {
+
+    public static void main(String[] args) {
+
+        // Final = Anything that is Final can not be change once you declare the variable
+//         double pi = 3.141567;
+//
+//        System.out.println(pi);
+
+        final double PI = 3.141567;
+
+//        PI = 4; //java: cannot assign a value to final variable pi
+        System.out.println(PI);
+    }
+}
